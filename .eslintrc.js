bcd13788f3e4f8db08a2d67c063ff3d047fe1576
Harry <harrychenhc@icloud.com>
2021-02-25 20:53:21 -0500

    fix eslint error for "delete cr"

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,9 @@ module.exports = {
   env: {
     browser: true,
   },
-  rules: {},
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
   overrides: [
     // node files
     {
